1034e8aca51c585cb3e63c43d5eee2c92b820329
stineaussi <stineandersson05@gmail.com>
2018-06-13 15:09:26 +0200

    mangler to bokse

--- source/main.js
+++ source/main.js
@@ -228,14 +228,39 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";  
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace("active", "");
-  }
   slides[slideIndex-1].style.display = "block";  
-  dots = [slideIndex-1].className += "active";
 }
 
+function countdown(){
 
+    // Set the date we're counting down to
+let countDownDate = new Date("Sep 5, 3000 22:00:00").getTime();
 
+// Update the count down every 1 second
+let x = setInterval(function() {
 
+    // Get todays date and time
+    let now = new Date().getTime();
+    
+    // Find the distance between now an the count down date
+    let distance = countDownDate - now;
+    
+    // Time calculations for days, hours, minutes and seconds
+    //var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    // Output the result in an element with id="demo"
+    document.getElementById("closingtime").innerHTML = hours + "h "
+    + minutes + "m " + seconds + "s ";
+    
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("closingtime").innerHTML = "EXPIRED";
+    }
+}, 1000);
 
+}
+countdown();
